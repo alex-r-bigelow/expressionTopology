5a3b2c2bd12e2af0e743648fdd6f7e5cd62f27b0
yasashiku <supamu.no.akaunto@gmail.com>
2013-04-15 14:27:52 -0600

    prototype 2 working

--- prototype2.py
+++ prototype2.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import argparse, sys, math
-from PySide.QtGui import QApplication, QGraphicsScene, QGraphicsItem, QPen, QFont, QBrush, QCompleter
-from PySide.QtCore import Qt, QFile, QRectF
+from PySide.QtGui import QApplication, QGraphicsScene, QGraphicsItem, QPen, QFont, QBrush, QCompleter, QTableWidgetItem, QColor, QColorDialog
+from PySide.QtCore import Qt, QFile, QRectF, QTimer
 from PySide.QtUiTools import QUiLoader
 from soft import SoftFile
-
-def to_5_sig_figs(x):
-    # TODO
-    return str(x)
 
 class parametricPulseGraph(QGraphicsItem):
     COLOR_MAP = {}
@@ -16,21 +12,24 @@ class parametricPulseGraph(QGraphicsItem):
     MOUSED_CATEGORY = None
     MOUSED_PEN = QPen(Qt.white,THICKNESS)
     
-    SLICE_DELTA = 0.1
+    SLICE_DELTA = 0.05
     SLICE_START = 0.0
-    SLICE_END = 0.2
+    SLICE_END = 0.3
     UNSLICED_OPACITY = 0.5
     SLICED_OPACITY = 0.75
     
     FULL_SIZE = 300
     SMALL_SIZE = 100
     
-    LABEL_THRESHOLD = 200
+    LABEL_THRESHOLD = 150
     
-    TEXT_FONT = QFont('Helvetica', 9)
-    TEXT_PEN = QPen(Qt.gray)
+    TEXT_FONT = QFont('Helvetica', 11)
+    TEXT_PEN = QPen(Qt.white)
     TEXT_OPACITY = 0.5
-    BACKGROUND = QBrush(Qt.lightGray)
+    
+    BORDER_WIDTH = 2.0
+    BORDER = QPen(Qt.black,BORDER_WIDTH)
+    BACKGROUND = QBrush(QColor(20,20,20,255))
     
     def __init__(self, parent, xAttribute, yAttribute, vectors=None):
         QGraphicsItem.__init__(self)
@@ -48,56 +47,62 @@ class parametricPulseGraph(QGraphicsItem):
         
         if self.vectors == None:
             return
-        for x0,y0,x1,y1 in self.vectors.itervalues():
-            if self.xmin == None:
-                self.xmin = min(x0,x1)
-                self.xmax = max(x0,x1)
-                self.ymin = min(y0,y1)
-                self.ymax = max(y0,y1)
-            else:
-                self.xmin = min(self.xmin,x0,x1)
-                self.xmax = max(self.xmax,x0,x1)
-                self.ymin = min(self.ymin,y0,y1)
-                self.ymax = max(self.ymax,y0,y1)
+        for vlist in self.vectors.itervalues():
+            for x0,y0,x1,y1 in vlist:
+                if self.xmin == None:
+                    self.xmin = min(x0,x1)
+                    self.xmax = max(x0,x1)
+                    self.ymin = min(y0,y1)
+                    self.ymax = max(y0,y1)
+                else:
+                    self.xmin = min(self.xmin,x0,x1)
+                    self.xmax = max(self.xmax,x0,x1)
+                    self.ymin = min(self.ymin,y0,y1)
+                    self.ymax = max(self.ymax,y0,y1)
     
     def paint(self, painter, option, widget=None):
         normalRect = QRectF(0,0,self.width,self.height)
-        rotatedRect = QRectF(0,-self.height,self.width,0)
+        rotatedRect = QRectF(0,-self.height,self.width,self.height)
         
-        painter.fillRect(normalRect,parametricPulseGraph.BACKGROUND)
+        painter.setPen(parametricPulseGraph.BORDER)
+        painter.drawRect(normalRect)
         if self.vectors != None:
+            painter.fillRect(normalRect,parametricPulseGraph.BACKGROUND)
+            
             # Draw vectors
-            for cat,pen in parametricPulseGraph.COLOR_MAP.iteritems():
+            for cat,color in parametricPulseGraph.COLOR_MAP.iteritems():
+                if not self.vectors.has_key(cat):
+                    continue
                 if cat == parametricPulseGraph.MOUSED_CATEGORY:
                     painter.setPen(parametricPulseGraph.MOUSED_PEN)
                 else:
-                    painter.setPen(pen)
+                    painter.setPen(QPen(color,parametricPulseGraph.THICKNESS))
                 xScale = self.width / (self.xmax-self.xmin)
                 yScale = self.height / (self.ymax-self.ymin)
-                x0,y0,x1,y1 = self.vectors[cat]
-                x0 = (x0-self.xmin)*xScale
-                y0 = (y0-self.ymin)*yScale
-                x1 = (x1-self.xmin)*xScale
-                y1 = (y1-self.ymin)*yScale
-                painter.setOpacity(parametricPulseGraph.UNSLICED_OPACITY)
-                painter.drawLine(x0,y0,x1,y1)
-                xstart = (x1-x0)*parametricPulseGraph.SLICE_START
-                xend = (x1-x0)*parametricPulseGraph.SLICE_END
-                ystart = (y1-y0)*parametricPulseGraph.SLICE_START
-                yend = (y1-y0)*parametricPulseGraph.SLICE_END
-                painter.setOpacity(parametricPulseGraph.SLICED_OPACITY)
-                painter.drawLine(x0+xstart,y0+ystart,x0+xend,y0+yend)
+                for x0,y0,x1,y1 in self.vectors[cat]:
+                    x0 = (x0-self.xmin)*xScale
+                    y0 = (y0-self.ymin)*yScale
+                    x1 = (x1-self.xmin)*xScale
+                    y1 = (y1-self.ymin)*yScale
+                    painter.setOpacity(parametricPulseGraph.UNSLICED_OPACITY)
+                    painter.drawLine(x0,y0,x1,y1)
+                    xstart = (x1-x0)*parametricPulseGraph.SLICE_START
+                    xend = (x1-x0)*parametricPulseGraph.SLICE_END
+                    ystart = (y1-y0)*parametricPulseGraph.SLICE_START
+                    yend = (y1-y0)*parametricPulseGraph.SLICE_END
+                    painter.setOpacity(parametricPulseGraph.SLICED_OPACITY)
+                    painter.drawLine(x0+xstart,y0+ystart,x0+xend,y0+yend)
                 
             # Draw labels
             if min(self.width,self.height) >= parametricPulseGraph.LABEL_THRESHOLD:
                 painter.setFont(parametricPulseGraph.TEXT_FONT)
                 painter.setPen(parametricPulseGraph.TEXT_PEN)
                 painter.setOpacity(parametricPulseGraph.TEXT_OPACITY)
-                painter.drawText(normalRect,Qt.AlignCenter | Qt.AlignTop, to_5_sig_figs(self.ymax))
-                painter.drawText(normalRect,Qt.AlignCenter | Qt.AlignBottom, to_5_sig_figs(self.ymin))
+                painter.drawText(normalRect,Qt.AlignHCenter | Qt.AlignTop, "%s, %f" % (self.yAttribute,self.ymax))
+                painter.drawText(normalRect,Qt.AlignHCenter | Qt.AlignBottom, "%s, %f" % (self.yAttribute,self.ymin))
                 painter.rotate(90)
-                painter.drawText(rotatedRect,Qt.AlignCenter | Qt.AlignTop, to_5_sig_figs(self.xmax))
-                painter.drawText(rotatedRect,Qt.AlignCenter | Qt.AlignBottom, to_5_sig_figs(self.xmin))
+                painter.drawText(rotatedRect,Qt.AlignHCenter | Qt.AlignTop, "%s, %f" % (self.xAttribute,self.xmax))
+                painter.drawText(rotatedRect,Qt.AlignHCenter | Qt.AlignBottom, "%s, %f" % (self.xAttribute,self.xmin))
     
     @staticmethod
     def updateValues():
@@ -107,12 +112,14 @@ class parametricPulseGraph(QGraphicsItem):
         parametricPulseGraph.SLICE_END = parametricPulseGraph.SLICE_START+parametricPulseGraph.SLICE_DELTA
     
     def boundingRect(self):
-        return QRectF(self.x(),self.y(),self.width,self.height)
-    
-    def mouseReleaseEvent(self, event):
+        pensize = parametricPulseGraph.BORDER_WIDTH/2
+        return QRectF(-pensize,-pensize,self.width+pensize,self.height+pensize)
+        
+    def mousePressEvent(self, event):
         self.parent.setTarget(self.xAttribute,self.yAttribute)
 
 class multiViewPanel:
+    FRAME_DURATION = 1000/60 # 60 FPS
     def __init__(self, view, data):
         self.view = view
         self.data = data
@@ -123,13 +130,20 @@ class multiViewPanel:
         self.variableOrder = []
         self.currentX = None
         self.currentY = None
+        
+        # Update timer
+        self.timer = QTimer(self.view)
+        self.timer.timeout.connect(self.updateValues)
+        self.timer.start(multiViewPanel.FRAME_DURATION)
     
     def addVariable(self, var):
         if not var in self.variables.iterkeys():
-            varGraphs = {var:parametricPulseGraph(self,var,var,None)}    # stick in a placeholder for the diagonal
+            p = parametricPulseGraph(self,var,var,None)
+            self.scene.addItem(p)
+            varGraphs = {var:p}    # stick in a placeholder for the diagonal
             for v,graphs in self.variables.iteritems():
                 x = parametricPulseGraph(self,var,v,self.data.getVectors(var,v))
-                y = parametricPulseGraph(self,var,v,self.data.getVectors(v,var))
+                y = parametricPulseGraph(self,v,var,self.data.getVectors(v,var))
                 self.scene.addItem(x)
                 self.scene.addItem(y)
                 varGraphs[v] = x
@@ -140,11 +154,16 @@ class multiViewPanel:
     
     def removeVariable(self, var):
         self.variableOrder.remove(var)
+        for v in self.variables[var].itervalues():
+            self.scene.removeItem(v)
         del self.variables[var]
         for v in self.variableOrder:
+            self.scene.removeItem(self.variables[v][var])
             del self.variables[v][var]
         if self.currentX == var or self.currentY == var:
             self.noTarget()
+        else:
+            self.updateView()
     
     def noTarget(self):
         self.currentX = None
@@ -152,70 +171,45 @@ class multiViewPanel:
         self.updateView()
     
     def setTarget(self, x, y):
-        self.currentX = x
-        self.currentY = y
-        self.updateView()
+        if self.currentX == x and self.currentY == y:
+            self.noTarget()
+        else:
+            self.currentX = x
+            self.currentY = y
+            self.updateView()
     
     def updateView(self):
-        # TODO: get these sizes dynamically
-        xMax = 600
-        yMax = 600
-        if self.currentX == None and self.currentY == None:
-            if len(self.variables) == 1:
-                xPadding = max(0,(xMax-parametricPulseGraph.SMALL_SIZE)/2)
-                yPadding = max(0,(yMax-parametricPulseGraph.SMALL_SIZE)/2)
-            else:
-                xPadding = max(0,(xMax-parametricPulseGraph.SMALL_SIZE*len(self.variables))/(2*len(self.variables)))
-                yPadding = max(0,(yMax-parametricPulseGraph.SMALL_SIZE*len(self.variables))/(2*len(self.variables)))
-            for i,x in enumerate(self.variableOrder):
-                for j,y in enumerate(self.variableOrder):
-                    self.variables[x][y].setPos((2*i+1)*xPadding,(2*j+1)*yPadding)
+        padding = (parametricPulseGraph.FULL_SIZE-parametricPulseGraph.SMALL_SIZE)/2
+        xPos = 0
+        for x in self.variableOrder:
+            if x == self.currentX:
+                xPos += padding
+            
+            yPos = 0
+            for y in self.variableOrder:
+                if x == self.currentX and y == self.currentY:
+                    self.variables[x][y].setPos(xPos-padding,yPos)
+                    self.variables[x][y].width = parametricPulseGraph.FULL_SIZE
+                    self.variables[x][y].height = parametricPulseGraph.FULL_SIZE
+                    yPos += parametricPulseGraph.FULL_SIZE
+                else:
+                    if y == self.currentY:
+                        yPos += padding
+                    self.variables[x][y].setPos(xPos,yPos)
                     self.variables[x][y].width = parametricPulseGraph.SMALL_SIZE
                     self.variables[x][y].height = parametricPulseGraph.SMALL_SIZE
-        elif len(self.variables) == 1:
-            assert self.currentX == self.variableOrder[0] and self.currentY == self.variableOrder[0]
-            xPadding = max(0,(xMax-parametricPulseGraph.FULL_SIZE)/2)
-            yPadding = max(0,(yMax-parametricPulseGraph.FULL_SIZE)/2)
-            self.variables[self.currentX][self.currentY].setPos(xPadding,yPadding)
-            self.variables[self.currentX][self.currentY].width = parametricPulseGraph.FULL_SIZE
-            self.variables[self.currentX][self.currentY].height = parametricPulseGraph.FULL_SIZE
-        else:
-            others=len(self.variables)-1
-            xPadding = max(0,(xMax-parametricPulseGraph.FULL_SIZE-others*parametricPulseGraph.SMALL_SIZE)/(2*others))
-            yPadding = max(0,(yMax-parametricPulseGraph.FULL_SIZE-others*parametricPulseGraph.SMALL_SIZE)/(2*others))
+                    yPos += parametricPulseGraph.SMALL_SIZE
+                    if y == self.currentY:
+                        yPos += padding
             
-            specialPadding = (parametricPulseGraph.FULL_SIZE-parametricPulseGraph.SMALL_SIZE)/2
-            
-            passedX = False
-            for i,x in enumerate(self.variableOrder):
-                if not passedX:
-                    left = i*parametricPulseGraph.SMALL_SIZE + 2*i*xPadding
-                else:
-                    left = (i-1)*parametricPulseGraph.SMALL_SIZE + 2*(i-1)*xPadding + parametricPulseGraph.FULL_SIZE
-                if x != self.currentX:
-                    left += xPadding
-                else:
-                    left += specialPadding
-                passedY = False
-                for j,y in enumerate(self.variableOrder):
-                    if not passedY:
-                        top = j*parametricPulseGraph.SMALL_SIZE + 2*j*yPadding
-                    else:
-                        top = (j-1)*parametricPulseGraph.SMALL_SIZE + 2*(j-1)*yPadding + parametricPulseGraph.FULL_SIZE
-                    if y != self.currentY:
-                        top += yPadding
-                    else:
-                        top += specialPadding
-                    
-                    if y == self.currentY and x == self.currentX:
-                        # in the case we're actually at the target graph, we don't want any padding
-                        self.variables[x][y].setPos(left-specialPadding,top-specialPadding)
-                        self.variables[x][y].width = parametricPulseGraph.FULL_SIZE
-                        self.variables[x][y].height = parametricPulseGraph.FULL_SIZE
-                    else:
-                        self.variables[x][y].setPos(left,top)
-                        self.variables[x][y].width = parametricPulseGraph.SMALL_SIZE
-                        self.variables[x][y].height = parametricPulseGraph.SMALL_SIZE
+            xPos += parametricPulseGraph.SMALL_SIZE
+            if x == self.currentX:
+                xPos += padding
+        self.scene.update()
+    
+    def updateValues(self):
+        parametricPulseGraph.updateValues()
+        self.scene.update()
 class Viz:
     def __init__(self, data):
         self.data = data
@@ -225,27 +219,51 @@ class Viz:
         infile.open(QFile.ReadOnly)
         self.window = self.loader.load(infile, None)
         infile.close()
-                
-        self.window.geneBox.addItems(self.data.geneList)
-        completer = QCompleter(self.data.geneList)
+        
+        # Gene box
+        allGenes = self.data.geneList()
+        self.window.geneBox.addItems(allGenes)
+        completer = QCompleter(allGenes)
         completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.window.geneBox.setCompleter(completer)
         
         self.window.geneBox.clearEditText()
         self.window.addButton.setEnabled(False)
         
+        # Color picker
+        self.allClasses = self.data.classList()
+        self.window.categoryTable.setRowCount(len(self.allClasses))
+        self.window.categoryTable.setColumnCount(2)
+        parametricPulseGraph.COLOR_MAP = dict(zip(self.allClasses,[Qt.blue for x in xrange(len(self.allClasses))]))
+        
+        for r,c in enumerate(self.allClasses):
+            self.window.categoryTable.setItem(r,0,QTableWidgetItem(c))
+            cItem = QTableWidgetItem("")
+            cItem.setBackground(QBrush(parametricPulseGraph.COLOR_MAP[c]))
+            self.window.categoryTable.setItem(r,1,cItem)
+        
+        self.window.categoryTable.cellClicked.connect(self.changeColor)
+        
+        # Main view
         self.multiPanel = multiViewPanel(self.window.graphicsView,self.data)
         
+        # Button events
         self.window.quitButton.clicked.connect(self.window.close)
         self.window.addButton.clicked.connect(self.addGene)
         self.window.geneBox.editTextChanged.connect(self.editGene)
         
-        #self.window.showFullScreen()
-        self.window.show()
+        self.window.showFullScreen()
+        #self.window.show()
+    
+    def changeColor(self, row, column):
+        if column == 1:
+            col = QColorDialog.getColor()
+            parametricPulseGraph.COLOR_MAP[self.window.categoryTable.item(row,0).text()] = col
+            self.window.categoryTable.item(row,1).setBackground(col)
     
     def addGene(self):
         g = self.window.geneBox.currentText()
-        if g in self.data.geneList:
+        if g in self.data.genes:
             t = self.window.addButton.text()
             if t == 'Add':
                 self.multiPanel.addVariable(g)
@@ -256,7 +274,7 @@ class Viz:
     
     def editGene(self):
         g = self.window.geneBox.currentText()
-        if g in self.data.geneList:
+        if g in self.data.genes:
             if self.multiPanel.variables.has_key(g):
                 self.window.addButton.setText('Remove')
             else:
--- soft.py
+++ soft.py
@@ -1,23 +1,143 @@
-import random
+import random, re, os, sys
 
+class softVector:
+    timeKeys = [re.compile(r'(?:(?P<hours>\d+)\s*h[ours]*)?\s*\,*(?:(?P<minutes>\d+)\s*m[inutes]*)?\s*\,*(?:(?P<seconds>\d+)\s*s[econds]*)?', re.I)]    # will match strings like '12 min 8SECONDS' or '2 m'
+    def __init__(self):
+        self.timepoint = None
+        self.descriptions = []
+        
+        self.nextPoint = None
+        self.previousPoint = None
+        
+        self.values = {}
+    
+    def addDescription(self, d):
+        setTimepoint = False
+        for r in softVector.timeKeys:
+            m = re.match(r,d).groupdict()
+            numSeconds = 0
+            if m.get('seconds',None) != None:
+                numSeconds += float(m['seconds'])
+            if m.get('minutes',None) != None:
+                numSeconds += float(m['minutes'])*60
+            if m.get('hours',None) != None:
+                numSeconds += float(m['hours'])*3600
+            if m.get('days',None) != None:
+                numSeconds += float(m['days'])*86400
+            if numSeconds > 0:
+                self.timepoint = numSeconds
+                setTimepoint = True
+                break
+        if not setTimepoint:
+            self.descriptions.append(d)
+    
+    def isRelated(self, other):
+        # Return true only if either no additional descriptions are shared or ALL descriptions are shared
+        for d in self.descriptions:
+            if not d in other.descriptions:
+                return False
+        return True
+    
+    def __cmp__(self, other):
+        if not isinstance(other,softVector) or self.timepoint == None or other.timepoint == None:
+            return -1
+        else:
+            return self.timepoint - other.timepoint
+    
 class SoftFile:
-    CLASSES = ['a','b','c']
     def __init__(self, path):
+        self.data = {}
+        self.genes = []
+        self.starts = {}
+        
         with open(path,'rb') as infile:
+            currentDesc = None
+            
+            headerOrder = None
+            families = []
+                        
             for line in infile:
-                pass
+                if line.startswith('!subset_description'):
+                    line = line.strip()
+                    currentDesc = line[line.find('=')+2:]
+                elif line.startswith('!subset_sample_id'):
+                    line = line.strip()
+                    line = line[line.find('=')+2:]
+                    assert currentDesc != None
+                    for h in line.split(','):
+                        if not self.data.has_key(h):
+                            self.data[h] = softVector()
+                        self.data[h].addDescription(currentDesc)
+                elif len(line) <= 1 or line.startswith('#') or line.startswith('!'):
+                    continue
+                elif line.startswith('^'):
+                    currentDesc = None
+                elif headerOrder == None:
+                    # Group families of data
+                    for hObj in self.data.itervalues():
+                        foundFamily = False
+                        for members in families:
+                            if members[0].isRelated(hObj):
+                                members.append(hObj)
+                                foundFamily = True
+                                break
+                        if not foundFamily:
+                            families.append([hObj])
+                    # Sort and link families
+                    for members in families:
+                        prevObj = None
+                        for hObj in sorted(members):
+                            if prevObj != None:
+                                hObj.previousPoint = prevObj
+                                prevObj.nextPoint = hObj
+                            prevObj = hObj
+                    headerOrder = line.split()[2:]
+                    # Get the starting points for each description
+                    for h,hObj in self.data.iteritems():
+                        for d in hObj.descriptions:
+                            startingPoint = hObj
+                            while startingPoint.previousPoint != None:
+                                startingPoint = startingPoint.previousPoint
+                            if self.starts.has_key(d):
+                                assert (self.starts[d].timepoint == None and startingPoint.timepoint == None) or self.starts[d] == startingPoint
+                            else:
+                                self.starts[d] = startingPoint
+                else:
+                    columns = line.split()
+                    gene = columns[1]
+                    if gene == 'EMPTY':
+                        continue
+                    self.genes.append(gene)
+                    for i,h in enumerate(headerOrder):
+                        if columns[i+2] != 'null':
+                            self.data[h].values[gene] = float(columns[i+2])
         infile.close()
-        self.geneList = ['gene1','gene2','mygene3']
     
     def getVectors(self, attr1, attr2):
         vectors = {}
-        lastx = 0
-        lasty = 0
-        for x in xrange(10):
-            for y in xrange(10):
-                newX = random.random()
-                newY = random.randint(-10,10)
-                vectors[random.choice(SoftFile.CLASSES)] = (lastx,lasty,lastx+newX,lasty+newY)
-                lastx += newX
-                lasty += newY
+        for d,hObj in self.starts.iteritems():
+            vectors[d] = []
+            x = hObj.values.get(attr1)
+            y = hObj.values.get(attr2)
+            if hObj.nextPoint == None:
+                if x != None and y != None:
+                    vectors[d].append((x,y,x,y))
+            else:
+                lastX = x
+                lastY = y
+                currentObj = hObj
+                while currentObj.nextPoint != None:
+                    currentObj = currentObj.nextPoint
+                    x = currentObj.values.get(attr1)
+                    y = currentObj.values.get(attr2)
+                    if lastX != None and lastY != None and x != None and y != None:
+                        vectors[d].append((lastX,lastY,x,y))
+                    lastX = x
+                    lastY = y
         return vectors
+    
+    def geneList(self):
+        return list(self.genes)
+    
+    def classList(self):
+        return self.starts.keys()
